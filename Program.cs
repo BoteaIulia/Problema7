2b5bd4a703090d728cf05d4e15cf36f0b43968d9
botea <botea@IULIA>
2022-11-30 13:24:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public static class Globals
+{
+	internal static void Main()
+	{
+		Console.Write("SWAP - Introduceti valorile variabilelor:");
+		Console.Write("\n");
+		int aux;
+		int a;
+		int b;
+		Console.Write("a = ");
+		a  = Convert.ToInt32(Console.ReadLine());
+		Console.Write("b = ");
+		b = Convert.ToInt32(Console.ReadLine());
+		aux = a;
+		a = b;
+		b = aux;
+		Console.Write("Valorile lor schimbate:");
+		Console.Write("\n");
+		Console.Write("a = ");
+		Console.Write(a);
+		Console.Write(" ");
+		Console.Write("b = ");
+		Console.Write(b);
+	}
+}
